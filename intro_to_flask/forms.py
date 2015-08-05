b5afde0820fdacffc665ee4a72db3e0fd3abf7f0
tkstman <tks.tman@gmail.com>
2015-08-04 19:58:45 -0500

    contactforminprog

--- intro_to_flask/forms.py
+++ intro_to_flask/forms.py
@@ -1,5 +1,7 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField
- 
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
 class ContactForm(Form):
   name = TextField("Name")
   email = TextField("Email")
---
+++ intro_to_flask/main.py
@@ -0,0 +1,128 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+This file creates your application.
+"""
+
+from intro_to_flask import app
+from flask import Flask
+from flask import render_template, request, redirect, url_for
+from models import db
+from forms import ContactForm
+
+app = Flask(__name__)
+
+app.secret_key = "tkssmartkodecodeWorldProdigy232323421@1127@6206birthd#2342)2**("
+
+#from flask.ext.mysqldb import MySQL
+#app = Flask(__name__)
+
+
+#mysql = MySQL(app)
+
+@app.route('/testdb/')
+def testdb():
+    if db.session.query("1").from_statement("SELECT 1").all():
+	return 'it works'
+    else:
+	return 'something is broke'
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
+###
+# Routing for your application.
+###
+
+@app.route('/')
+def home(name=None):
+    """Render the website's home page."""
+    return render_template('home2.html',name=name)
+    
+
+@app.route('/about/')
+def about(name=None):
+    """Render the website's about page."""
+    return render_template('about.html')
+
+@app.route('/profile/')
+def profile(name=None):
+    """Render the website's profile page."""
+    return render_template('profile.html',name=name)
+    
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    """Render the website's contact page."""
+    form = ContactForm()
+    
+    if request.method == 'POST':
+        return 'Form Submitted.'
+    elif request.method == 'GET':
+        return render_template('contact.html',form=form)
+
+
+
+@app.route('/profile/edit/')
+def profileedit(name=None):
+    """Render the website's edit profile page."""
+    return render_template('profileedit.html',name=name)
+    
+
+
+@app.route('/login/')
+def login(name=None):
+    """Render the website's login page."""
+    return render_template('login.html',name=name)
+    #
+@app.route('/report/')
+def report(name=None):
+    """Render the website's about page."""
+    return render_template('report.html',name=name)
+
+@app.route('/report/edit/')
+def reportadd(name=None):
+    """Render the website's about page."""
+    return render_template('reportedit.html',name=name)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  form = SignupForm()
+   
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      new_officer = Officer(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+      db.session.add(new_officer)
+      db.session.commit()
+       
+      return "[1] Create a new user [2] sign in the user [3] redirect to the user's profile"
+   
+  elif request.method == 'GET':
+    return render_template('signup.html', form=form)
+ ###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
